d03cfa27ac5f61fb00e0f3f915033e387f92f38b
Diana Steakley-Freeman <dev@dianasteakleyfreeman.com>
2019-08-19 10:52:04 -0500

    update tests for NoticeObj

---
+++ test/NoticeObj.spec.js
@@ -0,0 +1,26 @@
+/* Test Object */
+const NoticeObj = {
+  type: 'early_termination_notice',
+  id: '1540964',
+  attributes: {
+    title: '20191699: Roper Technologies, Inc.; Robert D. Mattlin',
+    'transaction-number': '20191699',
+    'acquired-entities':
+      'CE Payroll Service, Inc., ComputerEase Software, Inc.',
+    'acquired-party': 'Robert D. Mattlin',
+    'acquiring-party': 'Roper Technologies, Inc.',
+    date: '2019-08-15',
+    created: '2019-08-16 08:24:13',
+    updated: '2019-08-16 08:24:13',
+    tags: [],
+  },
+  relationships: [],
+  meta: [],
+  links: {
+    self:
+      'https://www.ftc.gov/enforcement/premerger-notification-program/early-termination-notices/20191699',
+  },
+}
+
+
+module.exports = NoticeObj
--- test/setup.js
+++ test/setup.js
@@ -1,14 +1,4 @@
 /* eslint-disable */
-var jsdom = require('jsdom').jsdom;
 
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-  if (typeof global[property] === 'undefined') {
-    global[property] = document.defaultView[property];
-  }
-});
 
-global.navigator = {
-  userAgent: 'node.js'
-};
+global.window 
